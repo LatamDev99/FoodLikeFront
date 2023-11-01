d986af3e27f043c5c73b3e325a32e69afe7f95e4
sckarlet015 <sckarlet015@gmail.com>
2023-10-31 18:04:38 -0600

    validacion inicio de sesion

--- src/components/Cliente/Sesion/Sesion.jsx
+++ src/components/Cliente/Sesion/Sesion.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import styles from "./Sesion.module.css"
 import logo from "../../../img/logoFoodLike.png"
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 import Loading from '../Loading/Loading'
 import facebook from "../../../img/facebook-1-svgrepo-com.svg"
 import google from "../../../img/google-color-svgrepo-com.svg"
@@ -9,19 +9,29 @@ import twitter from "../../../img/twitter-svgrepo-com.svg"
 import { useDispatch } from "react-redux";
 import { datosUsuario } from '../../../actions'
 import axios from 'axios'
+import { validarCorreo } from './functions'
 
 
 const Sesion = () => {
-    const [loading, setLoading] = useState(true)
-    const [correo, setCorreo] = useState("")
-    const [contrasena, setContrasena] = useState("")
-    const dispatch = useDispatch();
-    useEffect(() => {
-        setTimeout(() => {
-            setLoading(false)
-        }, 1000);
-    })
-    const onClickContinuar = async () => {
+  const [loading, setLoading] = useState(true)
+  const [correo, setCorreo] = useState("")
+  const [contrasena, setContrasena] = useState("")
+  const [errors, setErrors] = useState({ correo: "", contraseña: "" })
+  const [hasErrors, setHasErrors] = useState(false);
+
+  const dispatch = useDispatch();
+  const history = useHistory();
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false)
+    }, 1000);
+  })
+  const onClickContinuar = async () => {
+    const hasErrors = Object.values(errors).some((error) => error !== "");
+    if (hasErrors) {
+      alert("Lo sentimos, aún hay errores. Por favor, revisa los datos e inténtalo nuevamente.");
+      return;
+    } else {
       let credenciales = {
         correo: correo,
         contrasena: contrasena
@@ -30,6 +40,7 @@ const Sesion = () => {
         let response = await axios.post("http://localhost:3001/cliente/sesion", credenciales)
         if (response.data.nombre && response.data.nombre.length > 0) {
           dispatch(datosUsuario(response.data))
+          history.push("/cliente")
           alert(`¡Hola, ${response.data.nombre}! Has iniciado sesión.`);
         }
         else {
@@ -40,33 +51,37 @@ const Sesion = () => {
         console.error(error);
       }
     }
-    
-    const onChangeCorreo = (e) => {
-        setCorreo(e.target.value)
-    }
-    const onChangeContrasena = (e) => {
-        setContrasena(e.target.value)
-    }
+  }
+
+  const onChangeCorreo = (e) => {
+    setCorreo(e.target.value);
+    const correoError = validarCorreo(e.target.value) ? "" : "El Correo no es Valido";
+    setErrors({ ...errors, correo: correoError });
+    setHasErrors(Object.values({ ...errors, correo: correoError }).some((error) => error !== ""));
+  };
+
+  const onChangeContrasena = (e) => {
+    setContrasena(e.target.value)
+  }
   return (
-    loading ? <Loading/> :
-    <div className={styles.conteiner}>
+    loading ? <Loading /> :
+      <div className={styles.conteiner}>
         <img src={logo} alt="" />
         <h3>Iniciar Sesion</h3>
         <span>Correo</span>
-        <input type="text" value={correo} onChange={onChangeCorreo}/>
+        <input type="text" value={correo} onChange={onChangeCorreo} />
+        <span className={styles.error}>{errors.correo}</span>
         <span>Contraseña</span>
-        <input type="password" value={contrasena} onChange={onChangeContrasena}/>
+        <input type="password" value={contrasena} onChange={onChangeContrasena} />
         <Link to={"/cliente/recuperar"}>Olvido la Contraseña?</Link>
         <Link to={"/cliente/registro"}>Registrarse</Link>
         <div className={styles.icons}>
-        <img className={styles.icon} src={facebook} alt="" />
-        <img className={styles.icon} src={google} alt="" />
-        <img className={styles.icon} src={twitter} alt="" />
+          <img className={styles.icon} src={facebook} alt="" />
+          <img className={styles.icon} src={google} alt="" />
+          <img className={styles.icon} src={twitter} alt="" />
         </div>
-       <Link to={"/cliente"}>
-       <button onClick={onClickContinuar}>Iniciar Sesion</button>
-       </Link>
-    </div>
+        <button onClick={onClickContinuar} disabled={hasErrors} className={styles.btn}>Iniciar Sesion</button>
+      </div>
   )
 }
 
