800024795f518ef23366bf12cf21a24612e5c59e
Giancarlo Moreno <yankomoreno1@gmail.com>
2023-11-09 20:50:25 -0500

    crear platillos

--- src/components/Restaurante/CrearPlatillo/CrearPlatillo.jsx
+++ src/components/Restaurante/CrearPlatillo/CrearPlatillo.jsx
@@ -1,21 +1,36 @@
 import { useSelector } from 'react-redux'
-import styles from './CrearPlatillo.module.css'
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min'
 import { useState } from 'react'
 
-
+import axios from "axios";
+import styles from './CrearPlatillo.module.css'
 
 const CrearPlatillo = () =>{
 
     const categoriaPlatillo = useSelector(state => state.categoriaPlatillo)
-    const restaurante = useSelector(state => state.restaurante)
 
-    const [platillo, setPlatillo] = useState({restauranteId: restaurante.id,
-        categoriaId:categoriaPlatillo.id})
+    const [platillo, setPlatillo] = useState({categoriaId:categoriaPlatillo.id})
     
     const history = useHistory()
 
-    console.log(platillo.id, restaurante.id)
+
+    const handleSubmit = async() =>{
+        let json = await axios.post(
+            `http://localhost:3001/platillo`,
+            platillo
+          )
+        if(json.data==true){
+            history.push("/restaurante")
+        }
+    }
+    
+
+    const handleChange = (e) => {
+        setPlatillo({
+          ...platillo,
+          [e.target.name]: e.target.value,
+        });
+    }
     
     const ButtonRegresa = () =>{
         history.push("/restaurante/agregarplatillos")
@@ -24,11 +39,60 @@ const CrearPlatillo = () =>{
     return(
         <div className={styles.container}>
             <button onClick={ButtonRegresa}>Atrás</button>
-            <h1>{platillo.nombre}</h1>
-            
+            <div>
+                <label>Nombre: </label>
+                <input
+                  type="text"
+                  name="nombre"
+                  onChange={handleChange}
+                />
+            </div>
+            <div>
+                <label>Descripción: </label>
+                <input
+                  type="text"
+                  name="descripcion"
+                  onChange={handleChange}
+                />
+            </div>
+            <div>
+                <label>Precio: </label>
+                <input
+                  type="text"
+                  name="precio"
+                  onChange={handleChange}
+                />
+            </div>
+            <div>
+                <label>Foto: </label>
+                <input
+                  type="text"
+                  name="foto"
+                  onChange={handleChange}
+                />
+            </div>
+            <div>
+                <label>Promo: </label>
+                <input
+                  type="text"
+                  name="promo"
+                  onChange={handleChange}
+                />
+            </div>
+            <div>
+                <label>Stock: </label>
+                <input
+                  type="text"
+                  name="stock"
+                  onChange={handleChange}
+                />
+            </div>
+
+            <div>
+                <button onClick={handleSubmit}>Crear Restaurante</button>
+              </div> 
 
 
-            
         </div>
     )
 }
