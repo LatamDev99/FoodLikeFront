af717bf9ba0cec7edff761e764c46a2563e3d2b6
Giancarlo Moreno <yankomoreno1@gmail.com>
2023-11-17 14:11:54 -0500

    administrar platillos  y editar platillo listos

--- src/components/Restaurante/AdministrarPlatillos/EditarPlatillo/EditarPlatillo.jsx
+++ src/components/Restaurante/AdministrarPlatillos/EditarPlatillo/EditarPlatillo.jsx
@@ -11,6 +11,13 @@ const Platillo = () =>{
     const [loading, setLoading] = useState(true)
 
     const platillo = useSelector(state => state.platilloEditar)
+
+    const [profileImage, setProfileImage] = useState("")
+    const [imagePreview, setImagePreview] = useState(null)
+    const [isLoading, setIsLoading] = useState(null)
+
+    const upload_preset = "images"
+
     const [plat, setPlat ] = useState(platillo)
     const history = useHistory()
 
@@ -20,6 +27,12 @@ const Platillo = () =>{
         history.push(`/restaurante/administrarplatillos`)
     }
 
+    const handleImageChangue = async(e) =>{
+        setProfileImage(e.target.files[0])
+        setImagePreview(URL.createObjectURL(e.target.files[0]))
+        setIsLoading(true)
+      }
+     
     const handleChange = (e) =>{
         setPlat({
             ...plat,
@@ -51,6 +64,38 @@ const Platillo = () =>{
           setLoading(false)
         }, 1000);
       },[])
+
+      useEffect(() => {
+        const uploadImageToCloudinary = async () => {
+          if (profileImage && ( 
+            profileImage.type === "image/png" ||  
+            profileImage.type === "image/jpg" ||
+            profileImage.type === "image/jpeg"      
+          )) {
+            const image = new FormData();
+            image.append("file", profileImage);
+            image.append("upload_preset", upload_preset);
+      
+            try {
+              const response = await axios.post("https://api.cloudinary.com/v1_1/dzhx3cwlp/image/upload", image);
+              
+              const imgData = response.data;
+              if (imgData.url) {
+                const imageURL = imgData.url.toString();
+                plat.foto = imageURL
+                setIsLoading(false);
+              } else {
+                console.error('Error al obtener la URL de la imagen desde Cloudinary:', imgData);
+              }
+            } catch (error) {
+              console.error('Error durante la carga de la imagen a Cloudinary:', error);
+              setIsLoading(false);
+            }
+          }
+        };
+      
+        uploadImageToCloudinary();
+      }, [profileImage]);
 
     return(
         loading ? <Loading /> :
@@ -85,12 +130,27 @@ const Platillo = () =>{
             </div>
             <div>          
                 <label>Foto: </label>
-                <input 
-                    type="text" 
-                    name="foto" 
-                    value={plat.foto}
-                    onChange={handleChange} />
+                <img src={plat.foto}/>
             </div>
+            <div>        
+                <p>
+                  <label>Foto:</label>
+                  <input type="file" accept='image/png, image/jpeg' name="foto" onChange={handleImageChangue}></input>
+                </p>
+              <p>
+              </p>
+                <div>
+                {isLoading !== null && (
+                  <div>
+                    {isLoading ? (
+                      <p>Cargando...</p>
+                    ) : (
+                      <p>Cargado</p>
+                    )}
+                  </div>
+                )}
+                </div>
+              </div>
             <div>          
                 <label>Promo: </label>
                 <input 
--- src/components/Restaurante/AdministrarPlatillos/Tabla/Tabla.jsx
+++ src/components/Restaurante/AdministrarPlatillos/Tabla/Tabla.jsx
@@ -10,45 +10,98 @@ const DataPlatillos = ({ data  }) => {
 
   const [dataPlatillos, setDataPlatillos] = useState(data)
 
+  const cat  = dataPlatillos.flatMap((categoria) =>
+  categoria.map(({ id, nombre }) => ({ id, nombre }))
+  )  
+  const [categoriasPlatillos, setCategoriasPlatillos] = useState(cat)
+
+  const dispatch = useDispatch()
+  const history = useHistory()
+  
+  const handleSubmit = (platillo) => {        
+          dispatch(editarPlatillo(platillo))
+          history.push(`/restaurante/editarplatillo`) 
+  }
 
 
-    const dispatch = useDispatch()
-    const history = useHistory()
-    
-    const handleSubmit = (platillo) => {        
-            dispatch(editarPlatillo(platillo))
-            history.push(`/restaurante/editarplatillo`) 
+  
+
+  const handleCategoria = async (e,  platilloId, categoriaSaliente ) => {
+
+    const categoriaSeleccionadaId = categoriasPlatillos[e.target.selectedIndex]?.id;
+
+    const a = {
+      idPlatillo: platilloId, 
+      idCategoriaEntrante: categoriaSeleccionadaId
     }
+      const response = await axios.patch(
+        `http://localhost:3001/categoriaplatillo/cambiarcategoria`,
+        a
+      ); 
 
-    const handleEliminarPlatillo = async (platillo) => {
+      if(response.status===200){
+        const newDataPlatillos = moverPlatilloACategoria(dataPlatillos, platilloId,categoriaSeleccionadaId);
+
+        setDataPlatillos(newDataPlatillos);
+      }
+  };
+
+  const moverPlatilloACategoria = (dataPlatillos, platilloId, categoriaSeleccionadaId) => {
+    let newData = [...dataPlatillos];
+
+    newData = newData.map((categoria) => {
+      return categoria.map((cat) => {
+        if (cat.Platillos) {
+          const platilloIndex = cat.Platillos.findIndex((platillo) => platillo.id === platilloId);
+          if (platilloIndex !== -1) {
+
+            const movedPlatillo = cat.Platillos[platilloIndex];
+            cat.Platillos.splice(platilloIndex, 1);
+
+            const targetCategoriaIndex = newData.findIndex(
+              (targetCat) => targetCat[0].id === categoriaSeleccionadaId
+            );
+            if (targetCategoriaIndex !== -1) {
+              newData[targetCategoriaIndex][0].Platillos.push(movedPlatillo);
+            }
+          }
+        }
+        return cat;
+      });
+    });
+    return newData;
+  };
+
+  const handleEliminarPlatillo = async (platillo) => {
       try {        
         const response = await axios.delete(`http://localhost:3001/platillo/eliminar/${platillo.id}`);
 
-        if (response.status === 200) {
-          const idPlatilloAEliminar = platillo.id.toString();
-        
-          setDataPlatillos((prevData) =>
-            prevData.map((subArray) =>
-              subArray.map((item) => {
-                if (item.Platillos) {
-                  return {
-                    ...item,
-                    Platillos: item.Platillos.filter(
-                      (platillo) => platillo.id !== idPlatilloAEliminar
-                    ),
-                  };
-                }
-                return item;
-              })
-            )
-          );
-        } else {
-          console.error('Error al eliminar el platillo. Estado del servidor:', response.status);
-        }
+          if (response.status === 200) {
+            const idPlatilloAEliminar = platillo.id.toString();
+          
+            setDataPlatillos((prevData) =>
+              prevData.map((subArray) =>
+                subArray.map((item) => {
+                  if (item.Platillos) {
+                    return {
+                      ...item,
+                      Platillos: item.Platillos.filter(
+                        (platillo) => platillo.id !== idPlatilloAEliminar
+                      ),
+                    };
+                  }
+                  return item;
+                })
+              )
+            );
+          } else {
+            console.error('Error al eliminar el platillo. Estado del servidor:', response.status);
+          }
       } catch (error) {
         console.error('Error al eliminar el platillo:', error);
       }
     };
+    
 
     const eliminarCategoria = async (id) => {
       console.log(id)
@@ -61,6 +114,7 @@ const DataPlatillos = ({ data  }) => {
       <div key={index} style={{ marginBottom: '20px' }}>
         <h2>{categoria[0]?.nombre} <button onClick={()=> eliminarCategoria(categoria[0]?.id)}>Eliminar</button></h2>       
         <table border="2">
+        {categoria[0].Platillos && categoria[0].Platillos.length > 0 ? (
           <thead>
             <tr>
               <th>Nombre</th>
@@ -71,11 +125,12 @@ const DataPlatillos = ({ data  }) => {
               <th>Stock</th>
               <th>Activo</th>
               <th>Configuración</th>
+              <th>Cambiar</th>
               <th>Eliminar</th>
             </tr>
           </thead>
+        ):""}
           <tbody>
-            {/* Verifica si existen platillos antes de mapear */}
             {categoria[0].Platillos && categoria[0].Platillos.length > 0 ? (
               categoria[0].Platillos?.map((platillo, platilloIndex) => (
                 <tr key={platilloIndex}>
@@ -89,6 +144,18 @@ const DataPlatillos = ({ data  }) => {
                   <td>
                     <button onClick={() => handleSubmit(platillo)}>Editar</button>
                   </td>
+                  <td>      
+      
+                  <select value={`${index}-${platillo.id}`} onChange={(e) => handleCategoria(e, platillo.id, categoria[0].id)}>
+                  {categoriasPlatillos?.map((categoriaItem, itemIndex) => (
+                      <option key={categoriaItem.id} value={`${itemIndex}-${platillo.id}`}>
+                          {categoriaItem.nombre}
+                      </option>
+                  ))}
+                </select>
+
+
+                  </td>
                   <td>
                     <button onClick={() => handleEliminarPlatillo(platillo)}>Eliminar</button>
                   </td>
@@ -96,7 +163,7 @@ const DataPlatillos = ({ data  }) => {
               ))
             ) : (
               <tr>
-                <td colSpan="9">No hay platillos disponibles</td>
+                <td colSpan="10">No hay platillos disponibles</td>
               </tr>
             )}
           </tbody>
